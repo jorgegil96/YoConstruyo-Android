4442c9e3685d2ac14ef1f1e61007df7f9ad7c185
jorgegil96 <jorgegilcavazos@gmail.com>
2017-03-21 11:08:28 -0600

    Add dependencies and API service

---
+++ app/src/main/java/mx/com/cdcs/yoconstruyo/data/service/YoConstruyoService.java
@@ -0,0 +1,23 @@
+package mx.com.cdcs.yoconstruyo.data.service;
+
+import io.reactivex.Single;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface YoConstruyoService {
+
+    /*
+    @GET("modules.json")
+    Single<List<Module>> fetchModules();
+    */
+
+    /*
+    @FormUrlEncoded
+    @POST("signup.json")
+    Single<SignUpResponse> signUp(@Field("email") String email,
+                                  @Field("password") String password);
+    */
+
+}
