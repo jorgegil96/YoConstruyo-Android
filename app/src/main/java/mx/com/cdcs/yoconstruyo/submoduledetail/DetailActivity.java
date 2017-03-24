a1c48c1b8045208460ba720aee42ac6d5e893d51
jorgegil96 <jorgegilcavazos@gmail.com>
2017-03-24 10:57:29 -0600

    Add DetailActivity

---
+++ app/src/main/java/mx/com/cdcs/yoconstruyo/submoduledetail/DetailActivity.java
@@ -0,0 +1,15 @@
+package mx.com.cdcs.yoconstruyo.submoduledetail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mx.com.cdcs.yoconstruyo.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
